d30c4f2ce32944cf0bccd888f37eace13efdafba
iherotesttools@gmail.com <iherotesttools@gmail.com@45321775-b5fc-ee0f-2e26-a374fb40c672>
2014-12-10 12:10:03 +0000

    ARTI v1.1.1

---
+++ ARTI/SessionFiles/Scripts/GenerateConfigClassesCSharp.bat
@@ -0,0 +1,3 @@
+SET XSD="C:\Program Files\Microsoft Visual Studio 8\SDK\v2.0\Bin\Xsd.exe"
+%XSD% TesttoolConfig.xsd /namespace:TesttoolConfigurationEditor /classes /type:TesttoolConfig
+pause
---
+++ ARTI/SessionFiles/Scripts/GenerateConfigClassesVB.bat
@@ -0,0 +1,3 @@
+SET XSD="C:\Program Files\Microsoft Visual Studio 8\SDK\v2.0\Bin\Xsd.exe"
+%XSD% TesttoolConfig.xsd /classes /type:TesttoolConfig /language:VB
+pause
