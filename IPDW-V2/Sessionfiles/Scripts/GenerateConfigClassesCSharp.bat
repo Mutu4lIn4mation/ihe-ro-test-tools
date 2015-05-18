505ab714beed689e92b2860f37d0be9cd126c415
iherotesttools@gmail.com <iherotesttools@gmail.com@45321775-b5fc-ee0f-2e26-a374fb40c672>
2015-05-18 13:19:52 +0000

    TDW-II release 1.0

--- IPDW-V2/Sessionfiles/Scripts/GenerateConfigClassesCSharp.bat
+++
@@ -1,3 +0,0 @@
-SET XSD="C:\Program Files (x86)\Microsoft SDKs\Windows\v8.0A\bin\NETFX 4.0 Tools\Xsd.exe"
-%XSD% TesttoolConfig.xsd /namespace:TesttoolConfigurationEditor /classes /type:TesttoolConfig
-pause
