03c91d8fa518d8c867b467b4298743770a143fc6
iherotesttools@gmail.com <iherotesttools@gmail.com@45321775-b5fc-ee0f-2e26-a374fb40c672>
2015-05-18 13:05:20 +0000

    QAPV release 1.2

--- ARTI/SessionFiles/Scripts/GenerateConfigClassesCSharp.bat
+++
@@ -1,3 +0,0 @@
-SET XSD="C:\Program Files\Microsoft Visual Studio 8\SDK\v2.0\Bin\Xsd.exe"
-%XSD% TesttoolConfig.xsd /namespace:TesttoolConfigurationEditor /classes /type:TesttoolConfig
-pause
--- ARTI/SessionFiles/Scripts/GenerateConfigClassesVB.bat
+++
@@ -1,3 +0,0 @@
-SET XSD="C:\Program Files\Microsoft Visual Studio 8\SDK\v2.0\Bin\Xsd.exe"
-%XSD% TesttoolConfig.xsd /classes /type:TesttoolConfig /language:VB
-pause
